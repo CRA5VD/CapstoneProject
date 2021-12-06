c8a2d1645704a213d8b41195b3ef86bab2907c95
Connie <constancecrawford87@gmail.com>
2021-12-06 15:11:53 -0500

    cap

---
+++ client/src/components/About.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import bannerPic from '../img/washpark.jpg'
+
+
+class About extends Component{
+    render() {
+      return (
+      <div className="main-contain">
+        <div className="banner-container">
+          <div className="banner-container-sub">
+            <img className="banner-bg" src={bannerPic} alt="Logo" />
+          </div>
+        </div>
+          <div className='post card'>
+            <div className="card-content">
+              <h2>About</h2>
+              <p>Insert</p>
+            </div>
+          </div>
+      </div>
+      )
+    }    
+}
+
+export default (About)
+
+
