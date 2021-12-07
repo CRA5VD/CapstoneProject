b1c9876f98343599754ab78c00c45f39a3f11423
Connie <constancecrawford87@gmail.com>
2021-12-07 17:34:49 -0500

    Cap

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import Popper from '@popperjs/core';
 import 'bootstrap-icons/font/bootstrap-icons.css';
