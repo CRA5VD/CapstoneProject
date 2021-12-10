dae417d6ade309fee32f7aa192b46e48457c07dc
Corbett <corbett.croslin@gmail.com>
2021-12-10 13:47:55 -0500

    recursively removed node_modules

--- client/src/components/FoodDrinks.js
+++ client/src/components/FoodDrinks.js
@@ -6,9 +6,23 @@ import axios from 'axios'
 import {BACKEND_URL} from '../config'
 const FoodDrink = (props) => {
   return (
-    <div className="bg-primary" style={{backgroundColor: 'red'}}>
-      <img src={props.fooddrinks.imageURL} alt="" />
+    <div className="main-contain">
+   
+    <div className="events-main-contain container-fluid justify-content-center d-flex mt-5 w-100">
+      <Card style={{ width: '35rem', height: '40rem' }}>
+        <Card.Img variant="top" src={props.fooddrink.imageURL} />
+        <Card.Body>
+          <Card.Title><h2>{props.fooddrink.name}</h2></Card.Title>
+            <Card.Text>
+              <h3>{props.fooddrink.area}</h3>
+              <h4>{props.fooddrink.address}</h4>
+              <p>{props.fooddrink.description}</p>
+          </Card.Text>
+          <Button variant="primary">Website</Button>
+        </Card.Body>
+      </Card>
     </div>
+  </div>
   )
   
 }
@@ -23,7 +37,7 @@ class FoodDrinks extends Component{
     }
   }
   componentDidMount(){
-    axios.get(BACKEND_URL + 'fooddrinks/')
+    axios.get(BACKEND_URL + 'fooddrink/')
     .then(response => {
       this.setState({
         fooddrink : response.data,
@@ -44,34 +58,21 @@ class FoodDrinks extends Component{
       return (
         this.state.loading === false ? (
           <div className="">
+            <div className="banner-contain">
+      <div className="banner-sub">
+      <img className="banner-bg" src={bannerPic} alt="Logo" />
+      </div>
+    </div> 
+
             {this.fooddrinkList()}
           </div>
         ) : (
           <div className="">
             <h1>Loading...</h1>
+          
           </div>
         )
-      // <div className="main-contain">
-      //   <div className="banner-contain">
-      //     <div className="banner-sub">
-      //     <img className="banner-bg" src={bannerPic} alt="Logo" />
-      //     </div>
-      //   </div>  
-      //   <div className="events-main-contain container-fluid justify-content-center d-flex mt-5 w-100">
-      //     <Card style={{ width: '35rem', height: '40rem' }}>
-      //       <Card.Img variant="top" src="holder.js/100px180" />
-      //       <Card.Body>
-      //         <Card.Title><h2>Card Title</h2></Card.Title>
-      //           <Card.Text>
-      //             <h3>Area</h3>
-      //             <h4>Address</h4>
-      //             <p>Description</p>
-      //         </Card.Text>
-      //         <Button variant="primary">Website</Button>
-      //       </Card.Body>
-      //     </Card>
-      //   </div>
-      // </div>
+    
       )
     }    
 }
