391a918eaa254975929a2bcccc21f56af982b03c
Connie <constancecrawford87@gmail.com>
2021-12-13 18:00:36 -0500

    capStone

--- client/src/components/EntertainmentSports.js
+++ client/src/components/EntertainmentSports.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import bannerPic from '../img/BLINKRoeblingBridge.jpg'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
+import {BACKEND_URL} from '../config'
 
-class EntertainmentSports extends Component{
-    render() {
+const EntertainmentSports = (props) => {
       return (
       <div className="main-contain">
         <div className="banner-contain">
@@ -29,6 +30,59 @@ class EntertainmentSports extends Component{
       </div>
       )
     }    
-}
+    class EntertainmentSports extends Component{
+      constructor(props){
+        super(props)
+        this.state = {
+          entertainmentsports: [],
+          loading: true
+        }
+      }
+      componentDidMount(){
+        axios.get(BACKEND_URL + 'entertainmentsports/')
+        .then(response => {
+          this.setState({
+            entertainmentsports : response.data,
+            loading: false
+          })
+          console.log('entertainmentsports')
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+      }
+      entertainmentsportsList() {
+        return this.state.entertainmentsports.map((currentFood) => {
+          return <EntertainmentSports entertainmentsports = {currentFood} key={currentFood._id} />
+        })
+      }
+        render() {
+          return (
+            this.state.loading === false ? (
+              <div className="">
+                <div className="banner-contain">
+                  <div className="banner-sub">
+                  <img className="banner-bg" src={bannerPic} alt="Logo" />
+                </div>
+              </div> 
+                <div className="container-fluid">
+                  <div className="row justify-content-center">
+                    <div className="col-6">
+                      {this.entertainmentsportsList()}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="">
+                <h1>Loading...</h1>
+              
+              </div>
+            )
+        
+          )
+        }    
+    }
+    
 
-export default (EntertainmentSports)
+export default EntertainmentSports
